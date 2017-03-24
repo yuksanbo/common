d9479a5e740543233ca9287316aaedde32684e35
Yuri Geinish <wilemyvu@yandex.ru>
2017-03-24 12:09:55 +0300

    String.extract()'s preAnchor can be null now.

--- misc/src/main/kotlin/ru/yuksanbo/common/misc/StringExt.kt
+++ misc/src/main/kotlin/ru/yuksanbo/common/misc/StringExt.kt
@@ -1,9 +1,20 @@
 package ru.yuksanbo.common.misc
 
-fun String.extract(preAnchor: String, postAnchor: String? = null): String? {
-    val preIdx = this.indexOf(preAnchor)
-    if (preIdx == -1) return null
-    val preEndIdx = preIdx + preAnchor.length
+/**
+ * Alternative to regex group extraction for simple cases (x10 faster).
+ * @param preAnchor if null, extract starting at `this[0]`
+ * @param postAnchor if null, extract until `this[this.length]`
+ * @return substring between [preAnchor] and [postAnchor]
+ */
+fun String.extract(preAnchor: String? = null, postAnchor: String? = null): String? {
+    val preIdx: Int
+    if (preAnchor != null) {
+        preIdx = this.indexOf(preAnchor)
+        if (preIdx == -1) return null
+    } else {
+        preIdx = 0
+    }
+    val preEndIdx = preIdx + (preAnchor?.length ?: 0)
 
     val postIdx: Int
     if (postAnchor != null) {
